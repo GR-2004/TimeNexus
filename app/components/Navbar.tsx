4c8e98f7a134c0eff7e21b4bb3bba32c355dd236
GR-2004 <ganeshrana6377772039@gmail.com>
2025-03-16 21:10:47 +0530

    implement auth and auth.js

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
-import Logo from "@/public/logo.png"
+import Logo from "@/public/logo.png";
+import { Button } from "@/components/ui/button";
+import { AuthModal } from "./AuthModal";
 
-export function Navbar(){
-    return (
-        <div className="flex py-5 items-center justify-between bg-red-500">
-            <Link href="/">
-                <Image src={Logo} alt="Logo" className="size-10" />
-            </Link>
-        </div>
-    )
+export function Navbar() {
+  return (
+    <div className="flex py-5 items-center justify-between">
+      <Link href="/" className="flex items-center gap-2">
+        <Image src={Logo} alt="Logo" className="size-10" />
+        <h4 className="text-3xl font-semibold">
+          Time<span className="text-blue-500">Nexus</span>
+        </h4>
+      </Link>
+      <AuthModal />
+    </div>
+  );
 }
