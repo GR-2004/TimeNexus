519078c70624e20a6ce40ea23b63e2627c570bfc
GR-2004 <ganeshrana6377772039@gmail.com>
2025-03-19 10:17:28 +0530

    update dependencies and add availability management feature

--- app/action.ts
+++ app/action.ts
@@ -5,6 +5,7 @@ import { requireUser } from "./lib/hooks";
 import { parseWithZod } from "@conform-to/zod";
 import { onboardingSchemaValidation, settingSchema } from "./lib/zodSchema";
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
 export async function OnboardingAction(prevState: any, formData: FormData) {
   const session = await requireUser();
@@ -32,6 +33,47 @@ export async function OnboardingAction(prevState: any, formData: FormData) {
     data: {
       userName: submission.value.userName,
       name: submission.value.fullName,
+      availability: {
+        createMany: {
+          data: [
+            {
+              day: "Monday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Tuesday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Wednesday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Thursday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Friday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Saturday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+            {
+              day: "Sunday",
+              fromTime: "08:00",
+              tillTime: "18:00",
+            },
+          ],
+        },
+      },
     },
   });
   return redirect("/onboarding/grant-id");
@@ -59,4 +101,44 @@ export async function SettingsAction(prevState: any, formData: FormData) {
   });
 
   return redirect("/dashboard");
+}
+
+export async function updateAvailabilityAction(formData: FormData) {
+  const session = await requireUser();
+
+  const rawData = Object.fromEntries(formData.entries());
+
+  const availabilityData = Object.keys(rawData)
+    .filter((key) => key.startsWith("id-"))
+    .map((key) => {
+      const id = key.replace("id-", "");
+
+      return {
+        id,
+        isActive: rawData[`isActive-${id}`] === "on", //switch shadcn return on when true and undefined when false
+        fromTime: rawData[`fromTime-${id}`] as string,
+        tillTime: rawData[`tillTime-${id}`] as string,
+      };
+    });
+
+  try {
+    await prisma.$transaction(
+      availabilityData.map((item) =>
+        prisma.availability.update({
+          where: {
+            id: item.id,
+          },
+          data: {
+            isActive: item.isActive,
+            fromTime: item.fromTime,
+            tillTime: item.tillTime,
+          },
+        })
+      )
+    );
+
+    revalidatePath("/dashboard/availability");
+  } catch (error) {
+    console.error(error);
+  }
 }
