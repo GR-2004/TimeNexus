6ace58d127ace70ceceff7b82f3ea24ac6e7a5ec
GR-2004 <ganeshrana6377772039@gmail.com>
2025-03-22 10:56:02 +0530

    add key prop to form element in MeetingPage for improved rendering

--- app/dashboard/meetings/page.tsx
+++ app/dashboard/meetings/page.tsx
@@ -59,7 +59,7 @@ export default async function MeetingPage() {
           </CardHeader>
           <CardContent>
             {data.data.map((event) => (
-              <form action={CancelMeetingAction}>
+              <form action={CancelMeetingAction} key={event.id}>
                 <input type="hidden" name="eventId" value={event.id} />
                 <div className="grid grid-cols-3 justify-between items-center">
                   <div>
